c43be650ceb82aba5ad5c9b5e47a343a76564883
Matas Goštautas <32739863+MatasGos@users.noreply.github.com>
2024-06-26 03:44:21 +0300

    moved TLS config

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"context"
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"encoding/pem"
 	"fmt"
 	"github.com/quic-go/quic-go"
 	"log"
-	"math/big"
+	"pubsubQUIC/config"
 	"pubsubQUIC/pubsub"
 )
 
@@ -28,7 +24,7 @@ func main() {
 }
 
 func publisherServer(a *pubsub.Agent[messageType]) error {
-	listener, err := quic.ListenAddr(pubAddr, generateTLSConfig(), nil)
+	listener, err := quic.ListenAddr(pubAddr, config.GenerateTLSConfig(), nil)
 	if err != nil {
 		return err
 	}
@@ -63,7 +59,12 @@ func publisherServer(a *pubsub.Agent[messageType]) error {
 }
 
 func subscriberServer(a *pubsub.Agent[messageType]) error {
-	listener, err := quic.ListenAddr(subAddr, generateTLSConfig(), nil)
+	tlsConf := &tls.Config{
+		InsecureSkipVerify: true,
+		NextProtos:         []string{"quic-pub-sub"},
+	}
+
+	listener, err := quic.ListenAddr(subAddr, tlsConf, nil)
 	if err != nil {
 		return err
 	}
@@ -90,28 +91,5 @@ func subscriberServer(a *pubsub.Agent[messageType]) error {
 				}
 			}
 		}(conn)
-	}
-}
-
-func generateTLSConfig() *tls.Config {
-	key, err := rsa.GenerateKey(rand.Reader, 1024)
-	if err != nil {
-		panic(err)
-	}
-	template := x509.Certificate{SerialNumber: big.NewInt(1)}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		panic(err)
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		panic(err)
-	}
-	return &tls.Config{
-		Certificates: []tls.Certificate{tlsCert},
-		NextProtos:   []string{"quic-pub-sub"},
 	}
 }
