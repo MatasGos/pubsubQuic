3ad179827416d1838e0e03aa9c2da1c6d795de9b
Matas Goštautas <32739863+MatasGos@users.noreply.github.com>
2024-06-27 23:16:12 +0300

    update tests for new agent quit channel

--- pubsub/pubsub_test.go
+++ pubsub/pubsub_test.go
@@ -8,14 +8,14 @@ import (
 )
 
 func TestSubscribe(t *testing.T) {
-	agent := NewAgent[string]()
+	agent := NewAgent[string](nil)
 	sub := agent.AddSubscriber(0, nil)
 	require.Equal(t, 1, len(agent.subs))
 	require.NotNil(t, sub)
 }
 
 func TestBroadcast(t *testing.T) {
-	agent := NewAgent[string]()
+	agent := NewAgent[string](nil)
 	sub := agent.AddSubscriber(0, nil)
 	done := make(chan bool)
 	go func() {
@@ -36,21 +36,30 @@ func TestBroadcast(t *testing.T) {
 }
 
 func TestClosingConnections(t *testing.T) {
-	agent := NewAgent[string]()
+	agentQuitChan := make(chan struct{})
+	agent := NewAgent[string](agentQuitChan)
 	subQuitChan := make(chan struct{})
 	pubQuitChan := make(chan struct{})
 	sub := agent.AddSubscriber(0, subQuitChan)
 	pub := agent.AddPublisher(0, pubQuitChan)
+	pub2 := agent.AddPublisher(0, nil)
+	sub2 := agent.AddSubscriber(0, nil)
 
 	subQuitChan <- struct{}{}
 	pubQuitChan <- struct{}{}
 
 	require.Empty(t, sub)
 	require.Empty(t, pub)
+	require.NotNil(t, pub2)
+	require.NotNil(t, sub2)
+
+	agentQuitChan <- struct{}{}
+	require.Empty(t, pub2)
+	require.Empty(t, sub2)
 }
 
 func TestSubscriberNotification(t *testing.T) {
-	agent := NewAgent[string]()
+	agent := NewAgent[string](nil)
 	pub := agent.AddPublisher(0, nil)
 	agent.AddSubscriber(0, nil)
 	done := make(chan bool)
